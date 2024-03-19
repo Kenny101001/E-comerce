dcfa4884fe0c3f0e955ca93b0e9cca3f6b6e9e2c
Kenny101001 <kennylandhy@gmail.com>
2024-03-19 12:07:56 +0300

    c

--- our-products/snacks.php
+++ our-products/snacks.php
@@ -19,9 +19,33 @@ else {
 	<title>Snacks</title>
 	<link rel="stylesheet" type="text/css" href="../css/style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-5WSP7F65');</script>
+		<!-- End Google Tag Manager -->
+
+		<script src="/js/homeslideshow.js"></script>
+
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-7LY59282RY"></script>
+		<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-7LY59282RY');
+	</script>
 </head>
 
 <body>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WSP7F65"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	
 	<?php include ( "../inc/mainheader.inc.php" ); ?>
 	<div class="categolis">
 		<table>
