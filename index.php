3b01b97ef77cd6f1ea30cf1b79a9f1bcb5bc6cce
Kenny101001 <kennylandhy@gmail.com>
2024-03-19 10:11:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -19,18 +19,20 @@ else {
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-5WSP7F65');</script>
+		<!-- End Google Tag Manager -->
+
 		<script src="/js/homeslideshow.js"></script>
 	</head>
-	
-	<!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-5WSP7F65');</script>
-	<!-- End Google Tag Manager -->
 
 	<body style="min-width: 980px;">
+
 		<!-- Google Tag Manager (noscript) -->
 		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WSP7F65"
 		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
